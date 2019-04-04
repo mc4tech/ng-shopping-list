71daac3230c925122446b84a038e52ac7ae526b9
Meguel <chavezmeguel@gmail.com>
2019-04-03 22:07:11 -0400

    one way property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string;
+  name: string = 'Todd'
+  logo: string = './favicon.ico'
   constructor() {
     this.title = 'ng-shopping-list'
   }
