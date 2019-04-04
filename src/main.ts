59d2b3cf87b53cbe8ac552f9037864c73ee3f5a4
Meguel <chavezmeguel@gmail.com>
2019-04-03 21:06:40 -0400

    added remove method

--- src/main.ts
+++ src/main.ts
@@ -13,14 +13,22 @@
 
 function ShoppingList() {
   this.groceries = [];
-}
+};
 
 ShoppingList.prototype.addItem = function (item) {
   this.groceries = this.groceries.concat([item]);
-}
-
+};
+ShoppingList.prototype.removeItem = function (item) {
+  this.groceries = this.groceries.filter(function (grocery) {
+    return item !== grocery;
+  });
+};
 var myList = new ShoppingList();
 
 myList.addItem('Banana');
+myList.addItem('Bread');
 
 console.log(myList.groceries);
+
+myList.removeItem('Banana');
+console.log(myList.groceries)
