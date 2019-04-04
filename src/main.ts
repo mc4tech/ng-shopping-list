a8eb0f85fd53d9d40d742a4d36be8c3b65554cff
Meguel <chavezmeguel@gmail.com>
2019-04-03 21:49:49 -0400

    bootsrap main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,4 @@
-import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
-
-
-// previous code for initial intro 
-// class ShoppingList {
-//   groceries: string[];
-//   constructor() {
-//     this.groceries = []
-//   }
-
-//   addItem(item) {
-//     this.groceries = [...this.groceries, item];
-//   }
-
-//   removeItem(item) {
-//     this.groceries = this.groceries.filter((grocery) => item !== grocery);
-//   }
-// }
-
-// const myList = new ShoppingList();
-
-// myList.addItem('Bread');
-// myList.addItem('Pizza');
-
-// console.log(myList.groceries);
+platformBrowserDynamic().bootstrapModule(AppModule);
